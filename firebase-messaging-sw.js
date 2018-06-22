67535c4dd4ee1198d2b698de1014905718c9ad92
PCarval <pcarval@dialoginsight.com>
2018-06-22 09:27:44 -0400

    test

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,15 +1,117 @@
-// Give the service worker access to Firebase Messaging.
-// Note that you can only use Firebase Messaging here, other Firebase libraries
-// are not available in the service worker.
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+ importScripts('https://www.gstatic.com/firebasejs/4.5.0/firebase-app.js');
+ importScripts('https://www.gstatic.com/firebasejs/4.5.0/firebase-messaging.js');
 
-// Initialize the Firebase app in the service worker by passing in the
-// messagingSenderId.
-firebase.initializeApp({
-  'messagingSenderId': '327170440058'
+ firebase.initializeApp({
+   'messagingSenderId': '165900517247'
+ });
+
+ const messaging = firebase.messaging();
+ const applicationKey = '5992:QqTabDmZLj1kMmL30EaAwHkU1pI7g2fV';
+
+messaging.setBackgroundMessageHandler(function(payload) 
+{
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  return showNotification(payload);
 });
 
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
-const messaging = firebase.messaging();
+function showNotification(payload)
+{
+    var notificationTitle = payload.data.Title || 'Notification';
+    var notificationOptions = {
+    };
+
+    if(payload.data.Body)
+        notificationOptions.body = payload.data.Body;
+    if(payload.data.Title)
+        notificationOptions.title = payload.data.Title;
+    if(payload.data.URLRedirection)
+    {
+        if(!notificationOptions.data)
+                notificationOptions.data = {};
+            notificationOptions.data['url'] = payload.data.URLRedirection;
+    }
+    if(payload.data.Icon)
+        notificationOptions.icon = payload.data.Icon;
+    if(payload.data.Image)
+        notificationOptions.image = payload.data.Image;
+    if(payload.data.Direction)
+        notificationOptions.direction = payload.data.Direction;
+    if(payload.data.Sound)
+    {
+        notificationOptions.silent = false;
+        notificationOptions.sound = payload.data.Sound;
+    }
+    if(payload.data.Action1_Title)
+    {
+        if(!notificationOptions.data)
+            notificationOptions.data = {};
+        notificationOptions.actions = [];
+        var action1 = { action : 0, title : payload.data.Action1_Title };
+        if(payload.data.Action1_Icon)
+            action1['icon'] = payload.data.Action1_Icon;
+        notificationOptions.actions.push(action1);
+        if(payload.data.Action1_URL)
+            notificationOptions.data['action0_url'] = payload.data.Action1_URL;
+    }
+    if(payload.data.Action2_Title)
+    {
+        if(!notificationOptions.data)
+            notificationOptions.data = {};
+        if(!notificationOptions.actions)
+            notificationOptions.actions = [];
+        var action2 = { action : 1, title : payload.data.Action2_Title };
+        if(payload.data.Action2_Icon)
+            action2['icon'] = payload.data.Action2_Icon;
+        notificationOptions.actions.push(action2);
+        if(payload.data.Action2_URL)
+            notificationOptions.data['action1_url'] = payload.data.Action2_URL;
+    }
+
+    if(payload.data.OFSYSReceptionID)
+    {
+        var date = new Date();
+        var data = {
+            "ApplicationId": applicationKey,
+            "PushId": payload.data.OFSYSReceptionID
+        };
+
+        corsAjax({
+            "url" : 'https://lightspeed.dev.ofsys.com/webservices/ofc4/push.ashx?method=TrackPushReception', 
+            "data" : JSON.stringify(data)
+        });
+    }
+
+    return self.registration.showNotification(notificationTitle, notificationOptions);
+}
+
+function corsAjax(options)
+{
+    var headers = new Headers({"Content-Type": "text/plain"});
+    var body = options.data;
+    fetch(options.url, { mode : 'cors', method: 'POST', headers: headers, body: body}).then(function(response)
+    {
+        console.log(response);
+    });
+}
+
+self.addEventListener('notificationclick', function(event) 
+{
+    console.log(event);
+
+    var url = event.notification.data.url;
+    if(event.action != null && event.action != '')
+    {
+        url = event.notification.data['action' + event.action + '_url'];
+    }
+
+    event.notification.close();
+    event.waitUntil(clients.openWindow(url));
+});
+
+self.addEventListener('message', function (evt) {
+    console.log('postMessage received', evt.data);
+    if(typeof evt.data.showNotification !== 'undefined')
+    {
+        showNotification(evt.data.showNotification);
+    }
+});
